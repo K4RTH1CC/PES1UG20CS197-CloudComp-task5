32925df9e2b4a5ce9be543e887ef36a043f74c6d
Karthik Nair <karthik16092002@gmail.com>
2023-02-17 23:45:22 +0530

    switch to cpp

--- mainP/my_prog.c
+++
@@ -1,7 +0,0 @@
-#include<stdlib.h>
-#include<stdio.h>
-
-int main(){
-    printf("K4RTH1CC/PES1UG20CS197/CloudComputing/Experiment2a/task-5\n");
-    return 1;
-}
---
+++ mainP/my_prog.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "PES1UG20CS197/K4RTH1CC/CloudComputing/Experiment2a/Task5\n";
+  return 0;
+}
